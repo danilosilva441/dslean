924752da225653119d5093121cf8961d4f25cb85
Danilo da Silva Souza <danilosilva441@gmail.com>
2021-12-27 21:07:50 -0300

    Offer, Notification

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -13,4 +13,11 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
-INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpxhere.com%2Fen%2Fphoto%2F1443423&psig=AOvVaw3DbG6s9p1vrOcgKMcXGIFl&ust=1640726649812000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLjNlZ31hPUCFQAAAAAdAAAAABAD', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Ficon-library.com%2Ficon%2Fcourse-icon-19.html&psig=AOvVaw1XBoHeCpWAQ6qvtBlf9sv1&ust=1640726689162000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPj50LX1hPUCFQAAAAAdAAAAABAI'); 
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpxhere.com%2Fen%2Fphoto%2F1443423&psig=AOvVaw3DbG6s9p1vrOcgKMcXGIFl&ust=1640726649812000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLjNlZ31hPUCFQAAAAAdAAAAABAD', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Ficon-library.com%2Ficon%2Fcourse-icon-19.html&psig=AOvVaw1XBoHeCpWAQ6qvtBlf9sv1&ust=1640726689162000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPj50LX1hPUCFQAAAAAdAAAAABAI');
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp JAVA', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpxhere.com%2Fen%2Fphoto%2F1443423&psig=AOvVaw3DbG6s9p1vrOcgKMcXGIFl&ust=1640726649812000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLjNlZ31hPUCFQAAAAAdAAAAABAD', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Ficon-library.com%2Ficon%2Fcourse-icon-19.html&psig=AOvVaw1XBoHeCpWAQ6qvtBlf9sv1&ust=1640726689162000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPj50LX1hPUCFQAAAAAdAAAAABAI');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2021-12-27T03:00:00Z', TIMESTAMP WITH TIME ZONE '2022-12-27T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2022-01-05T03:00:00Z', TIMESTAMP WITH TIME ZONE '2023-01-05T03:00:00Z', 2);
+
+
+INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Bem vindo ao Bootcamp', TIMESTAMP WITH TIME ZONE '2022-01-05T03:00:00Z', false, 'Men', 3);
